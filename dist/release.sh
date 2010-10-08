41bc5b9b0dcf1b0bb70544388847f3388041a184
Nick Burch <nick@apache.org>
2010-10-08 10:51:48 +0000

    Add scripts to do the release, based on the ones produced by Apache LibCloud
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/cmislib/trunk@1005771 13f79535-47bb-0310-9956-ffa450edef68

---
+++ dist/release.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+# Did they give a user to sign with?
+user=""
+case "$1" in
+  -u)
+    shift
+    user="$1"
+    shift
+    ;;
+esac
+
+if test -z "${user}"; then
+   # Do they have a default user already set?
+   gpg_file=$HOME/.gnupg/gpg.conf
+   if test -f $gpg_file; then
+      if grep -q '^default-key' $gpg_file; then
+         user=`grep '^default-key' $gpg_file | awk '{print $2}'`
+         echo "Selected GPG default user of $user"
+      fi
+   fi
+fi
+
+if test -z "${user}"; then
+  echo "must pass -u with a gpg id"
+  exit 1
+fi
+
+cd ..
+
+python setup.py sdist  --formats=bztar,zip
+
+cd dist
+
+./hash-sign.sh -u ${user} *.tar.bz2 *.zip
