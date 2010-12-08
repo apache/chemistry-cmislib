5d010c2ab8ed1b7d25d977784a620ab8f0365cbf
Jeffrey Taylor Potts <jpotts@apache.org>
2010-12-08 02:21:48 +0000

    Adjusting run-sphinx so that it can be invoked from anywhere in the file system
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/cmislib/trunk@1043282 13f79535-47bb-0310-9956-ffa450edef68

--- src/doc/src/run-sphinx.py
+++ src/doc/src/run-sphinx.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python
 import os
-os.system("make html")
+import sys
+print sys.path[0]
+os.environ['BUILDDIR']=sys.path[0] + "/../build"
+os.environ['SOURCEDIR']=sys.path[0]
+os.system("make -e --makefile=" + sys.path[0] + "/Makefile html")
