2a0581c06ab807d681a39a19224ad1f9e74e06f2
Jeffrey Taylor Potts <jpotts@apache.org>
2010-12-08 21:08:59 +0000

    Overriding the Makefile with a version-free sphinx-build executable reference
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/cmislib/trunk@1043677 13f79535-47bb-0310-9956-ffa450edef68

--- src/doc/src/run-sphinx.py
+++ src/doc/src/run-sphinx.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 import sys
-print sys.path[0]
+os.environ['SPHINXBUILD']="sphinx-build"
 os.environ['BUILDDIR']=sys.path[0] + "/../build"
 os.environ['SOURCEDIR']=sys.path[0]
 os.system("make -e --makefile=" + sys.path[0] + "/Makefile html")
