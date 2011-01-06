4085a853db4283d714e414262b92bf6bd06689a8
Jeffrey Taylor Potts <jpotts@apache.org>
2011-01-06 20:50:40 +0000

    Modifying hudson sphinx script to force clean
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/cmislib/trunk@1056070 13f79535-47bb-0310-9956-ffa450edef68

--- src/doc/src/run-sphinx.py
+++ src/doc/src/run-sphinx.py
@@ -2,6 +2,10 @@
 import os
 import sys
 os.environ['SPHINXBUILD']="sphinx-build"
+build_dir = sys.path[0] + "/../build"
+os.environ['BUILDDIR']=build_dir
 os.environ['BUILDDIR']=sys.path[0] + "/../build"
 os.environ['SOURCEDIR']=sys.path[0]
+# force a clean every time
+os.system("rm -rf " + build_dir + "/*")
 os.system("make -e --makefile=" + sys.path[0] + "/Makefile html")
