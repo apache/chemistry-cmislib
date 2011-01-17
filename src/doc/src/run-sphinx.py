54743baf46cab0c85e8555b5d79b2df8c36b186f
Jeffrey Taylor Potts <jpotts@apache.org>
2011-01-17 17:38:19 +0000

    Tweaking doc build script
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/cmislib/trunk@1060020 13f79535-47bb-0310-9956-ffa450edef68

--- src/doc/src/run-sphinx.py
+++ src/doc/src/run-sphinx.py
@@ -4,8 +4,8 @@ import sys
 os.environ['SPHINXBUILD']="sphinx-build"
 build_dir = sys.path[0] + "/../build"
 os.environ['BUILDDIR']=build_dir
-os.environ['BUILDDIR']=sys.path[0] + "/../build"
 os.environ['SOURCEDIR']=sys.path[0]
 # force a clean every time
-os.system("rm -rf " + build_dir + "/*")
+print "Removing build dir: %s" % build_dir
+os.system("rm -rf " + build_dir)
 os.system("make -e --makefile=" + sys.path[0] + "/Makefile html")
