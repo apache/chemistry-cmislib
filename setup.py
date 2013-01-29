1c5215a77a4464816fba4dedabe04e2e38057b73
Jeffrey Taylor Potts <jpotts@apache.org>
2013-01-29 15:25:18 +0000

    Incrementing build number for cmislib
    git-svn-id: https://svn.apache.org/repos/asf/chemistry/cmislib/trunk@1439930 13f79535-47bb-0310-9956-ffa450edef68

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.5.1'
+version = '0.6'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
